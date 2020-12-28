f9f36ce61d8f29e56e7b8e991ac342f2691a6b96
мфтигорь <vanbujhm@gmail.com>
2020-12-28 12:23:40 +0300

    Update ph-load.js

--- ph-load.js
+++ ph-load.js
@@ -1,31 +1,63 @@
 function searchVideo(videoId){	
 	var flashvars = window.wrappedJSObject['flashvars_' + videoId];
+	var video240 = false;
+	var video480 = false;
+	var video720 = false;
+	var video1080 = false;
+
 	if(typeof flashvars !== 'undefined'){
-		if(typeof flashvars.quality_1080p !== 'undefined'){
-			return flashvars.quality_1080p;
-		}else if(typeof flashvars.quality_720p !== 'undefined'){
-			return flashvars.quality_720p;
-		}else if(typeof flashvars.quality_480p !== 'undefined'){
-			return flashvars.quality_480p;
-		}else if(typeof flashvars.quality_240p !== 'undefined'){
-			return flashvars.quality_240p;
+		if(typeof flashvars.mediaDefinitions !== 'undefined'){
+			for(var i = 0, len = flashvars.mediaDefinitions.length; i < len; i++){
+				var media = flashvars.mediaDefinitions[i];
+				if(media.format == 'mp4'){
+					if(media.quality == '240'){
+						video240 = media.videoUrl;
+					}else if(media.quality == '480'){
+						video480 = media.videoUrl;
+					}else if(media.quality == '720'){
+						video720 = media.videoUrl;
+					}else if(media.quality == '1080'){
+						video1080 = media.videoUrl;
+					}
+				}
+			}
+			
+			if(video1080 != false){
+				return video1080;
+			}else if(video720 != false){
+				return video720;
+			}else if(video480 != false){
+				return video480;
+			}else if(video240 != false){
+				return video240;
+			}
 		}
 	}
 	return false;
 }
 
-
 var flashvarsString;
 var video = false;
-var videoShow = window.wrappedJSObject['VIDEO_SHOW'];
-if(typeof videoShow.playerId !== 'undefined'){
-	flashvarsString = videoShow.playerId.replace('playerDiv_', '');
+var player = document.getElementById('videoShow');
+
+if(player !== null){
+	var videoElements = player.getElementsByTagName('video-element');
+	if(videoElements !== null){
+		flashvarsString = videoElements[0].firstChild.id;
+	}
 }
-else if(typeof videoShow.trackVideoId !== 'undefined'){
-	flashvarsString = videoShow.trackVideoId;
+if(typeof flashvarsString !== 'undefined'){
+	flashvarsString = flashvarsString.replace('playerDiv_', '');
 }
 else{
-	flashvarsString = false;
+	var videoShow = window.wrappedJSObject['VIDEO_SHOW'];
+	if(typeof videoShow.video_id !== 'undefined'){
+		flashvarsString = videoShow.video_id;
+	}
+	else{
+		flashvarsString = false;
+	}
 }
+
 video = searchVideo(flashvarsString);
 video;
